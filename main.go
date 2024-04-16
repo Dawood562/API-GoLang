d1ab92d865e404fc56bf308d22132a9caea53ac8
Dawood562 <dawooda0104@gmail.com>
2024-04-16 22:39:54 +0100

    Added a POST endpoint and a GET endpoint that take an ID as a parameter

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -29,12 +30,50 @@ func getItems(context *gin.Context) {
 	context.IndentedJSON(http.StatusOK, items)
 }
 
+func addItem(context *gin.Context) {
+	var newItem xyz
+
+	if err := context.BindJSON(&newItem); err != nil {
+		return
+	}
+
+	items = append(items, newItem)
+	context.IndentedJSON(http.StatusCreated, newItem)
+}
+
+func getItem(id string) (*xyz, error) {
+	for i, t := range items {
+		if t.Id == id {
+			return &items[i], nil
+		}
+	}
+	return nil, errors.New("Todo Not Found")
+}
+
+func getItemById(context *gin.Context) {
+	id := context.Param("id")
+	it, err := getItem(id)
+	if err != nil {
+		context.IndentedJSON(http.StatusNotFound, gin.H{"message": "Item not found."})
+		return
+	}
+
+	context.IndentedJSON(http.StatusOK, it)
+	return
+}
+
 func main() {
 	// Run a server
 	router := gin.Default()
 
 	// Set an endpoint to have a GET request
 	router.GET("/items", getItems)
+
+	// POST endpoint
+	router.POST("/addItem", addItem)
+
+	// GET endpoint, takes an argument
+	router.GET("/item/:id", getItemById)
 
 	// Run a server
 	router.Run("localhost:9000")
