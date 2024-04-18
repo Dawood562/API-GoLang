fdf271e12ab0df128fd282d4d1ee68ffb1d68441
Dawood562 <dawooda0104@gmail.com>
2024-04-18 22:51:52 +0100

    Remove caps and ending punctuation

--- database/get_item.go
+++ database/get_item.go
@@ -12,9 +12,9 @@ func GetItem(id string) (structs.Xyz, error) {
 
 	if err := rows.Scan(&thing.Id, &thing.Title, &thing.Description, &thing.Number, &thing.SomeBoolean); err != nil {
 		if err == sql.ErrNoRows {
-			return thing, errors.New("Item not found.")
+			return thing, errors.New("item not found")
 		}
-		return thing, errors.New("Error with turning records into struct.")
+		return thing, errors.New("error with turning records into struct")
 	}
 	return thing, nil
 }
